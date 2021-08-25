8f49bb665812edc0f148068b267586807b62e33b
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-08-25 17:54:10 +0300

    Add completed.js file

---
+++ src/completed.js
@@ -0,0 +1,68 @@
+const ul = document.getElementById('list-items');
+const checkbox = ul.getElementsByClassName('checkbox');
+const li = ul.getElementsByTagName('li');
+const p = ul.getElementsByTagName('p');
+const newTask = ul.getElementsByClassName('new-checkbox');
+const newList = ul.getElementsByClassName('new-list');
+const newParagraph = ul.getElementsByClassName('new-paragraph');
+
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+export default () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  checkbox.forEach((check, idcheck) => {
+    if (check.checked) {
+      li.forEach((list, idlist) => {
+        if (idlist == idcheck) {
+          list.className = 'delete';
+          p[idlist].style.textDecoration = 'line-through';
+          let i = list.id;
+          console.log(tasks.length);
+          tasks[i].isCompleted = true;
+          localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+      })
+    } else {
+      li.forEach((list, idlist) => {
+        if (idlist == idcheck) {
+          list.className = 'undelete';
+          p[idlist].style.textDecoration = 'none';
+          let i = list.id;
+          tasks[i].isCompleted = false;
+          localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+      })
+    }
+  })
+}
+
+export const newCompleted = () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  newTask.forEach((check, idcheck) => {
+    if (check.checked) {
+      newList.forEach((list, idlist) => {
+        if (idlist == idcheck) {
+          list.className = `new-list delete`;
+          newParagraph[idlist].style.textDecoration = 'line-through';
+          let i = list.id;
+          console.log(tasks);
+          console.log(list.id);
+          console.log(tasks.length);
+          tasks[i].isCompleted = true;
+          localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+      })
+    } else {
+      newList.forEach((list, idlist) => {
+        if (idlist == idcheck) {
+          list.className = 'new-list undelete';
+          newParagraph[idlist].style.textDecoration = 'none';
+          let i = list.id;
+          tasks[i].isCompleted = false;
+          localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+      })
+    }
+  })
+}
