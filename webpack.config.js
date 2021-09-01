f89e0dee8f5f8736b98ac41ec6fea81fad22c929
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-01 11:36:49 +0300

    Add path to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/ToDoList-JS-Project/",
+    publicPath: "/ToDoList-JS-Project/dist/",
     clean: true,
   },
   module: {
